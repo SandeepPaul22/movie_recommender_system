156196e68031953959abcd45fbc1ee26fe6fb68c
SandeepPaul22 <sandy2paul2@icloud.com>
2025-08-05 20:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,31 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+movies_file = st.file_uploader("Upload movies_list.pkl", type="pkl")
+similarity_file = st.file_uploader("Upload similarity.pkl", type="pkl")
+
+if not movies_file or not similarity_file:
+    st.warning("Please upload both 'movies_list.pkl' and 'similarity.pkl' files to continue.")
+    st.stop()
+
+# ✅ Fixed: Load from uploaded files, not local disk
+movies = pickle.load(movies_file)
+similarity = pickle.load(similarity_file)
+
+movies_list = movies['title'].values
 
 st.header("Movie Recommender System")
 
 import streamlit.components.v1 as components
 
 imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
 
 imageUrls = [
     fetch_poster(1632),
@@ -35,29 +43,25 @@ imageUrls = [
     fetch_poster(914),
     fetch_poster(255709),
     fetch_poster(572154)
-   
-    ]
-
+]
 
 imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movies_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
         recommend_poster.append(fetch_poster(movies_id))
     return recommend_movie, recommend_poster
 
-
-
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(movie_name[0])
         st.image(movie_poster[0])
